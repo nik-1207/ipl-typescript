a72540e6a677ce41e13c2e386c01b41b3b3881c6
nik-1207 <nmknikhilkashyap@gmail.com>
2021-09-21 12:20:27 +0530

    adding navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import NavStyle from "../styles/NavStyle";
+
+import LOGO from "../assets/ipl-logo.png";
+
+function Navbar() {
+  const style = NavStyle();
+  return (
+    <nav className={style.navbar}>
+      <img className={style.logoImage} src={LOGO} alt="ipl-logo" />
+      <h1 className={style.navHeading}>INDIAN PREMIRE LEAGUE</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/styles/NavStyle.ts
@@ -0,0 +1,23 @@
+
+import { createUseStyles } from "react-jss";
+
+const NavStyle= createUseStyles({
+  logoImage: {
+    height: "50px",
+    width: "40px",
+    margin: "15px",
+  },
+  navbar: {
+    backgroundColor: "#122a77",
+    display: "flex",
+    boxShadow: "0px 4px 8px hsla(0, 0%, 7%, .5)",
+    color: "white",
+    width: "100%",
+  },
+  navHeading: {
+    width: "min-content",
+    marginTop: "13px",
+    fontSize: "1em",
+  },
+});
+export default NavStyle;
