482134098c8d29deb871bf12628c50d1534e50cf
nik-1207 <nmknikhilkashyap@gmail.com>
2021-09-21 22:49:33 +0530

    adding spinner pubsub

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,21 @@
-import {  Route, BrowserRouter as Router, Switch} from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
-import './App.css';
-import Navbar from './components/NavBar';
-import AllTeams from "./pages/AllTeams"
-import TeamDetails from './pages/TeamDetails';
+import "./App.css";
+import Navbar from "./components/NavBar";
+import AllTeams from "./pages/AllTeams";
+import TeamDetails from "./pages/TeamDetails";
 
 function App() {
   return (
-        <div className="App">
-        <Navbar />
-        <Router>
+    <div className="App">
+      <Navbar />
+      <Router>
         <Switch>
-        <Route path="/" exact component={AllTeams}/>
-        <Route path="/teams/:url" exact component={TeamDetails}/>
+          <Route path="/" exact component={AllTeams} />
+          <Route path="/teams/:url" exact component={TeamDetails} />
         </Switch>
-        </Router>
-        </div>
-
+      </Router>
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CustomLoader from './components/CustomLoader';
+import Loader from 'react-loader-spinner';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+ <App />
+    <CustomLoader />
   </React.StrictMode>,
   document.getElementById('root')
 );
