55e95e8c0c6854a3c5bf3c804b5b799efedae71e
nik-1207 <nmknikhilkashyap@gmail.com>
2021-09-21 12:17:23 +0530

    adding navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
+      
     </div>
   );
 }
