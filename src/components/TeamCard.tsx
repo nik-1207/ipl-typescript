0c8eb681449f14eb259511c89f0f12b77acb9dbc
nik-1207 <nmknikhilkashyap@gmail.com>
2021-09-22 23:05:41 +0530

    adding interface to all components

--- src/components/TeamCard.tsx
+++ src/components/TeamCard.tsx
@@ -2,24 +2,38 @@ import { useCallback } from "react";
 import { useHistory } from "react-router-dom";
 import TeamCardStyle from "../styles/TeamCardStyle";
 import TeamCardColour from "../styles/TeamCardColour";
+interface teamDataType {
+  img: string;
+  id: string;
+  teamName: string;
+  venue: string;
+  winningYears: [number];
+}
+interface teamCardStyleType {
+  teamCard: string;
+  image: string;
+  tab: string;
+  teamName: string;
+  venue: string;
+  win: string;
+  teamHome: string;
+  arrow: string;
+}
+interface teamColourType {
+  [key: string]: string;
+}
+function Card(props: teamDataType) {
+  const { img, id, teamName, venue, winningYears } = props;
+  const history = useHistory<unknown>();
+  const style: teamCardStyleType = TeamCardStyle();
+  const colour: teamColourType = TeamCardColour();
 
-function Card(props:{img:string,id:string,teamName:string, venue:string, winningYears:[number]})
-{
-    
-  const { img, id,teamName,venue,winningYears } = props;
-  const history= useHistory<unknown>();
-  const style:{teamCard:string,image:string,tab:string,teamName:string,venue:string,win:string,teamHome:string,arrow:string} = TeamCardStyle();
-  const colour:{[key:string]:string}=TeamCardColour();
-
-  const clickHandler= useCallback(() => {
+  const clickHandler = useCallback(() => {
     history.push(`teams/${id}`);
   }, [history]);
 
   return (
-    <div
-      className={`${style.teamCard} ${colour[id]}`}
-      onClick={clickHandler}
-    >
+    <div className={`${style.teamCard} ${colour[id]}`} onClick={clickHandler}>
       <img className={style.image} src={img} alt="team-logo" />
       <div className={style.tab}>
         <h2 className={style.teamName}>{teamName}</h2>
@@ -27,7 +41,7 @@ function Card(props:{img:string,id:string,teamName:string, venue:string, winning
         {winningYears.length ? (
           <div className={style.win}>
             <i className="fas fa-trophy">
-              {winningYears.map((item:number) => {
+              {winningYears.map((item: number) => {
                 return `  ${item}  `;
               })}
             </i>
