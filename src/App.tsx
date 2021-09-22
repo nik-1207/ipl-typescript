47446232b5e998488dcd3b9b6037c879899b22a6
nik-1207 <nmknikhilkashyap@gmail.com>
2021-09-22 18:13:14 +0530

    adding lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
 import "./App.css";
 import Navbar from "./components/NavBar";
-import AllTeams from "./pages/AllTeams";
-import TeamDetails from "./pages/TeamDetails";
-
-function App() {
+import { Suspense, lazy } from 'react';
+import CustomLoader from "./components/CustomLoader";
+const AllTeams = lazy(() => import('./pages/AllTeams'));
+const TeamDetails = lazy(() => import('./pages/TeamDetails'));function App() {
   return (
     <div className="App">
       <Navbar />
       <Router>
+        <Suspense fallback={<CustomLoader/>}>
         <Switch>
           <Route path="/" exact component={AllTeams} />
           <Route path="/teams/:url" exact component={TeamDetails} />
         </Switch>
+        </Suspense>
       </Router>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import CustomLoader from './components/CustomLoader';
-import ErrorBoundry from './ErrorBoundry/ErrorBoundry';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import ErrorBoundry from "./ErrorBoundry/ErrorBoundry";
 
 ReactDOM.render(
   <React.StrictMode>
     <ErrorBoundry>
-    <App />
-    <CustomLoader />
+      <App />
     </ErrorBoundry>
-
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 reportWebVitals();
