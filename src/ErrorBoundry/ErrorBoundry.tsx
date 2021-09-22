a2b85a2a7f42c9bef1d9dd0de84d5c6b282b2acf
nik-1207 <nmknikhilkashyap@gmail.com>
2021-09-22 12:08:07 +0530

    type fixes player card ,navbar,customloader

---
+++ src/ErrorBoundry/ErrorBoundry.tsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+export default class ErrorBoundry extends Component {
+    state:{hasError:boolean}
+    constructor(props:any) {
+        super(props);
+        this.state = { hasError: false };
+      }
+    
+    static getDerivedStateFromError() {
+        return { hasError: true };
+      }
+    
+      render() {
+        if (this.state.hasError) 
+        {
+            return <h1>Something Went Wrong</h1>
+        }
+        return this.props.children
+
+    }
+}
