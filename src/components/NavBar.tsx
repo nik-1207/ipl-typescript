e358424491fe4d4c6e9b258c2ade965c146841c8
nik-1207 <nmknikhilkashyap@gmail.com>
2021-09-23 23:36:10 +0530

    improving accessibility

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,9 +6,9 @@ import LOGO from "../assets/ipl-logo.png";
 function Navbar() {
   const style = NavStyle();
   return (
-    <nav className={style.navbar}>
+    <nav title="navbar" className={style.navbar}>
       <img className={style.logoImage} src={LOGO} alt="ipl-logo" />
-      <h1 className={style.navHeading}>INDIAN PREMIRE LEAGUE</h1>
+      <h1 title="heading" className={style.navHeading}>INDIAN PREMIRE LEAGUE</h1>
     </nav>
   );
 }
